afdd7776adf05ef18fe3b821442a350e399d268c
Panupong Songsaksri <songskisri@gmail.com>
2025-08-08 14:54:05 +0700

    test cloudflareStorage

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -539,7 +539,7 @@ func (h *UserHandler) UpdateDocuments(c *fiber.Ctx) error {
 	uuid := uuid.New().String()
 	newName := fmt.Sprintf("%s/%s%s", req.Type, uuid, ext)
 
-	errOnUpload := h.upload.UploadFile(c.Context(), tempFilePath, newName)
+	errOnUpload := h.upload.UploadFileCloudflare(c.Context(), tempFilePath, newName)
 	if errOnUpload != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(dto.BaseResponse{
 			StatusCode: fiber.StatusInternalServerError,
